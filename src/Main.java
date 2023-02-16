f90cb8f7a77c5109717997641fb1f2c353bf65f2
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:43:10 -0500

    add car class

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
             //FOR INUTIL
             System.out.println(i);
         }
-        
+
         System.out.println("Hello world!");
         System.out.println(cars);
     }
